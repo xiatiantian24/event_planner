eaf10fdcf9c9295e6e9a5889a1b0dcfcf7864d2c
xiatiantian24 <xia.t@wustl.edu>
2024-07-12 12:48:13 +0800

    fin

---
+++ src/Components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EventPlanner.css'; // Import CSS file for styling
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
